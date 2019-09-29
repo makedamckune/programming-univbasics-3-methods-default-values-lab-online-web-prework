aba68298b34b45ac3d9b0ca5cfe3c9e7183d1d21
Makeda P Mckune <makedamckune@gmail.com>
2019-09-29 23:35:28 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,11 @@
-# Your code here
+puts "meal_choice1"
+meal_choice1 = gets 
+puts "meal_choice2"
+meal_choice2 = gets 
+puts "protien"
+protien = gets 
+puts "vegetable"
+vegtable = gets 
+
+puts = ("Hello there. We have a variety of different meals. What kind of meal would you like?" + meal_choice1 + "A choice of another?" + meal_choice2 + "A choice of" + protien + "or" + vegtable +)
+end
