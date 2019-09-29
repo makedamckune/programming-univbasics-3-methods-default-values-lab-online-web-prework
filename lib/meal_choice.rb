6d0b74ee685e12e7beface8b45521e683a77ba45
Makeda P Mckune <makedamckune@gmail.com>
2019-09-29 23:32:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,10 @@
 puts "meal_choice1"
 meal_choice1 = gets 
+puts "meal_choice2"
+meal_choice2 = gets 
+puts "protien"
+protien = gets 
+puts "vegetable"
+vegtable = gets 
+
+puts = ("Hello there. We have a variety of different meals. What kind of meal would you like? + meal_choice1")
